36f8d2e520f4fd29ae431ad759e1d0f88308567c
wienm <martin.scheibelreiter@chello.at>
2025-01-25 22:52:43 +0100

    entity, Sonderfall PersistenceCreator

---
+++ src/main/java/at/codecrafters/learnspringbootmongodb/entity/Student.java
@@ -0,0 +1,78 @@
+package at.codecrafters.learnspringbootmongodb.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceCreator;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.List;
+
+@Document(collection = "student")
+public class Student {
+
+    @Id
+    private String id;
+
+    //@Field(name="name")
+    private String name;
+
+    @Field(name="mail")
+    private String email;
+
+    private Department department;
+    private List<Subject> subjects;
+
+    @PersistenceCreator // vorher PersistenceConstractor // nur wenn mehrere
+    public Student(String id, String name, String email, Department department, List<Subject> subjects) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.department = department;
+        this.subjects = subjects;
+    }
+
+    public Student(String id, Department department) {
+        this.id = id;
+        this.department = department;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public List<Subject> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(List<Subject> subjects) {
+        this.subjects = subjects;
+    }
+}
---
+++ src/main/java/at/codecrafters/learnspringbootmongodb/entity/Subject.java
@@ -0,0 +1,27 @@
+package at.codecrafters.learnspringbootmongodb.entity;
+
+
+import org.springframework.data.mongodb.core.mapping.Field;
+
+public class Subject {
+    @Field(name="subject_name")
+    private String subjectName;
+    @Field(name="marks_obtained")
+    private String marksObtained;
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public String getMarksObtained() {
+        return marksObtained;
+    }
+
+    public void setMarksObtained(String marksObtained) {
+        this.marksObtained = marksObtained;
+    }
+}
