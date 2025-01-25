556156435f8e89690395eb7dcfa65402b82683a2
wienm <martin.scheibelreiter@chello.at>
2025-01-25 22:25:15 +0100

    init

---
+++ src/test/java/at/codecrafters/learnspringbootmongodb/LearnSpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package at.codecrafters.learnspringbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringbootMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
