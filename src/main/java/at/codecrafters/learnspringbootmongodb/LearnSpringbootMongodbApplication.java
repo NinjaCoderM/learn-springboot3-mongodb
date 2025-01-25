f91142ced25c9b2d832f82a79e72bb14924cac84
wienm <martin.scheibelreiter@chello.at>
2025-01-25 23:09:00 +0100

    repository

--- src/main/java/at/codecrafters/learnspringbootmongodb/LearnSpringbootMongodbApplication.java
+++ src/main/java/at/codecrafters/learnspringbootmongodb/LearnSpringbootMongodbApplication.java
@@ -2,8 +2,10 @@ package at.codecrafters.learnspringbootmongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+//@EnableMongoRepositories("at.codecrafters.learnspringbootmongodb.repository")
 public class LearnSpringbootMongodbApplication {
 
     public static void main(String[] args) {
