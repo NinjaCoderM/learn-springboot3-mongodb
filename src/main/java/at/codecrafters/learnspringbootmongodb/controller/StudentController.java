be5348c7322630ebc9ffd03b3f7fb7c6af33e5a8
wienm <martin.scheibelreiter@chello.at>
2025-01-26 11:46:48 +0100

    getById, getAllStudents

--- src/main/java/at/codecrafters/learnspringbootmongodb/controller/StudentController.java
+++ src/main/java/at/codecrafters/learnspringbootmongodb/controller/StudentController.java
@@ -3,10 +3,12 @@ package at.codecrafters.learnspringbootmongodb.controller;
 import at.codecrafters.learnspringbootmongodb.entity.Student;
 import at.codecrafters.learnspringbootmongodb.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/student")
@@ -19,5 +21,21 @@ public class StudentController {
     public Student createStudent(@RequestBody Student student) {
         return studentService.createStudent(student);
     }
+
+    @GetMapping("/getById/{id}")
+    public ResponseEntity<?> getStudentById(@PathVariable String id) {
+        Optional<Student> student = studentService.getStudentById(id);
+        if(student.isPresent()) {
+            return ResponseEntity.ok().body(student.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student with ID " + id + " not found");
+        }
+    }
+
+    @GetMapping("/all")
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
 
 }
--- src/main/java/at/codecrafters/learnspringbootmongodb/service/StudentService.java
+++ src/main/java/at/codecrafters/learnspringbootmongodb/service/StudentService.java
@@ -5,6 +5,9 @@ import at.codecrafters.learnspringbootmongodb.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class StudentService {
 
@@ -13,5 +16,13 @@ public class StudentService {
 
     public Student createStudent(Student student) {
         return studentRepository.save(student);
+    }
+
+    public Optional<Student> getStudentById(String id) {
+        return studentRepository.findById(id);
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
     }
 }
